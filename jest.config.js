611c6be2fa77d9c1da1040835930fa58771cd02d
Mark Ramy <markramyy@gmail.com>
2025-08-09 20:21:23 +0300

    Add jest dependencies for testing purposes

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  moduleNameMapping: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+};
---
+++ jest.setup.js
@@ -0,0 +1,10 @@
+// Jest setup file for global test configuration
+global.console = {
+  ...console,
+  // Uncomment to ignore console outputs during testing
+  // log: jest.fn(),
+  // debug: jest.fn(),
+  // info: jest.fn(),
+  // warn: jest.fn(),
+  // error: jest.fn(),
+};
